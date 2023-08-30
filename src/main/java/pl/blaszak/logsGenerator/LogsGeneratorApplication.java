95468508d482f40a56c33c8ad1d0bded4b853771
kblaszke <krzysztof.blaszke@gmail.com>
2023-08-30 21:11:20 +0200

    [feature/springBoot] SpringBoot starter init

---
+++ src/main/java/pl/blaszak/logsGenerator/LogsGeneratorApplication.java
@@ -0,0 +1,13 @@
+package pl.blaszak.logsGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/blaszak/logsGenerator/LogsGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.blaszak.logsGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
