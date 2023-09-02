83add86963f7bf006918a223f764fe4f4251c48e
kblaszke <krzysztof.blaszke@gmail.com>
2023-09-02 13:09:24 +0200

    [feature/logService] add logging

--- src/main/java/pl/blaszak/logsGenerator/service/LogsGeneratorService.java
+++ src/main/java/pl/blaszak/logsGenerator/service/LogsGeneratorService.java
@@ -1,7 +1,19 @@
 package pl.blaszak.logsGenerator.service;
 
+import jakarta.annotation.PreDestroy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LogsGeneratorService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogsGeneratorService.class);
+
+    @PreDestroy
+    public void closeService() {
+        LOGGER.info("Closing service");
+    }
+
     public void run() {
-        System.out.println("Run service");
+        LOGGER.info("Running service");
     }
 }
